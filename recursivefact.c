bda76945360987f827b5e49bcc54abb8c266fa15
Pranit Aher <86141506+pranit-aher@users.noreply.github.com>
2021-07-17 11:53:48 +0530

    Add files via upload

---
+++ recursivefact.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int fact(int n);
+int main()
+{
+	int n,f;
+	printf("Enter any No.:");
+	scanf("%d",&n);
+	
+	f=fact(n);
+	printf("\nFactorial of %d is %d",n,f);
+	
+}
+
+int fact(int n)
+{
+ int f;
+ 
+ if(n==0)
+ return 1;
+ 
+ 
+ else
+ {
+   f=n*fact(n-1); 
+
+ }
+}
